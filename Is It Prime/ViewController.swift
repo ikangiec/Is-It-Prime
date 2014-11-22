8697a37e6a6a06767c384ac7e627f00897cedb80
Kangie Chan <aries0420@gmail.com>
2014-11-21 22:40:10 -0800

    Basic app that finds prime numbers.

--- Is It Prime/ViewController.swift
+++ Is It Prime/ViewController.swift
@@ -10,6 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var number: UITextField!
+    @IBOutlet weak var message: UILabel!
+    @IBAction func buttonPressed(sender: AnyObject) {
+        var isPrime = true
+        
+        var Integer = number.text.toInt()
+        
+        if (Integer != nil) {
+            if (Integer < 1) {
+                message.text = "Please enter a positive number"
+            } else {
+                if (Integer == 1) {
+                    message.text = "1 is not a prime"
+                } else {
+                    for var i = 2; i < Integer; ++i {
+                        if (Integer! % i == 0) { // Number is not prime
+                            isPrime = false
+                        }
+                    }
+                    
+                    if (isPrime == true) {
+                        message.text = "That number is prime!"
+                    } else {
+                        message.text = "That number is not prime"
+                    }
+                }
+            }
+            
+        } else {
+            message.text = "Please enter a number"
+        }
+        
+        println(number.text)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
